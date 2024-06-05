665438295f1bd50a5908a0ae9cfe564f226821dd
deba443 <debaprasad443@gmail.com>
2024-06-05 23:40:46 +0530

    Fixed the counter part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState(10);
+  useEffect(() => {
+      if(value==0){
+        return
+      }
+      const timer=setTimeout(() => {
+        setValue((prev) => prev - 1);
+      }, 1000);
+      return () => clearTimeout(timer);
+  }, [value]);
   return (
     <div className="App">
-      <header className="App-header">
-        Counter
+      <header>
+        <h1>Counter</h1>
       </header>
+      <main>
+        <input className="input" value={value} />
+      </main>
     </div>
   );
 }
